3f1602d3faf26b499968365c9f9125a7a7a9d4ae
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2024-09-19 14:48:02 +0300

    fix

--- lab_2_java/CelToFahr.java
+++ lab_2_java/CelToFahr.java
@@ -4,10 +4,11 @@ public class CelToFahr {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите температуру в градусах Цельсия: ");
-        double celsius = scanner.nextDouble();
-        double fahrenheit = (celsius * 9 / 5) + 32;
-        System.out.printf("Эквивалент по Фаренгейту: %.2f\n", fahrenheit);
+        float celsius = scanner.nextFloat();
+        float fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Температура по Фаренгейту: " + fahrenheit);
     }
 }
+
 
 //task3
